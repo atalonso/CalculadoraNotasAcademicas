ed4e33fe5d2660208727aa88c7d2d975d8c5e20e
atalonso <adrianotalonso@gmail.com>
2020-08-31 21:20:33 -0300

    Programa que calcula as notas academicas.

---
+++ helpers.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    alunos = {
+        'jeffersons':
+            {'nome': 'Jefferson Santos',
+             'trabalhos': [90, 95, 80, 100],
+             'provas': [90, 80],
+             'laboratorio': [70, 85.2],
+             },
+        'pedrop':
+            {'nome': 'Pedro Pinheiro',
+             'trabalhos': [50, 75, 70,95],
+             'provas': [90, 80],
+             'laboratorio': [50,55.2],
+             },
+        'brunop':
+            {'nome': 'Bruno Pinheiro',
+             'trabalhos': [60, 45, 70, 90],
+             'provas': [90, 50],
+             'laboratorio': [50, 55.2],
+             },
+    }
